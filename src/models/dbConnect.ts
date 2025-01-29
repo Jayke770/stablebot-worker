16eccaaeed64f3808d70026aab1f0a9f14d2aca8
jayke770 <jakevelasco40@gmail.com>
2025-01-29 21:32:03 +0800

    .

--- src/models/dbConnect.ts
+++ src/models/dbConnect.ts
@@ -3,7 +3,26 @@ import mongoose from "mongoose";
 import { envconfig } from "../lib/config";
 async function dbConnect() {
     try {
-        return await mongoose.connect(envconfig.MONGODB_URI, { bufferCommands: true, });
+        const opts = {
+            bufferCommands: false,
+        };
+        if (envconfig.isDevelopment) {
+            if (cached.conn) {
+                return cached.conn;
+            }
+            if (!cached.promise) {
+                cached.promise = await mongoose.connect(envconfig.MONGODB_URI, opts);
+            }
+            try {
+                cached.conn = await cached.promise;
+            } catch (e) {
+                cached.promise = null;
+                throw e;
+            }
+        } else {
+            cached.promise = await mongoose.connect(envconfig.MONGODB_URI, opts);
+        }
+        return cached.conn;
     } catch (e) {
         console.log(e)
     }
